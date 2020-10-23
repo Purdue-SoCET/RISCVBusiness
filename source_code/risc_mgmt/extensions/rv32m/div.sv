472fc8f8b63444c3798f1d3b183e52ac1f930c72
Project32 SOC <socet32@asicfab.ecn.purdue.edu>
2020-10-22 21:55:39 -0400

    new

--- source_code/risc_mgmt/extensions/rv32m/div.sv
+++ source_code/risc_mgmt/extensions/rv32m/div.sv
@@ -1,70 +1,18 @@
-module div(input clk,
-input reset,
-input is_signed,
-input [31:0] dividend,
-input [31:0] divisor,
-output [31:0] quotient, 
-output [31:0] remainder
-);
-logic [31:0] usign_divisor, usign_dividend;
-logic adj_possible, adj_quo,adj_rem;
-
-assign usign_divisor = is_signed & divisor[31] ? (~divisor)+1: divisor;
-assign usign_divident = is_signed & dividend[31] ? (~divident)+1:dividend;
-assign adj_possible = is_signed && (divisor[31]^divident[31]);
-assign adj_quo = adj_possible && ~quotient[31];
-assign adj_rem = is_signed && divident[31];
-
-
-reg [31:0] r1,r2,r3,d2,d3,p,a;
-
-always_ff@(posedge clk, negedge reset)
-begin
-
-if (reset == 0) begin
-	quotient <= 0;
-	remainder <= 0;
-end
-
-else begin 
-	quotient <= a;
-	remainder <= p;
-end
-end
-
-always_comb begin
-
-a = usign_dividend;
-p = 0;
-d2 = usign_divisor << 1;
-d3 = usign_divisor << 1 + 1;
-
-for (i = 0; i < 16; i++) begin
-
-p = (p<<2) | a [63:62];
-a = a << 2;
-
-r1 = p - usign_divisor;
-r2 = p - d2;
-r3 = p - d3;
-
-if (r1[63])
-	a = a | 0;
-else if (r2[63]) begin
-	p = r1;
-	a = a | 1;
-end
-else if (r3[63]) begin 
-	p = r2;
-	a = a | 2;
-end
-else begin
-	p = r3;
-	a = a | 3;
-end
-end
-end
-endmodule
-
-
-
+irun(64): 15.20-s030: (c) Copyright 1995-2017 Cadence Design Systems, Inc.
+TOOL:	irun(64)	15.20-s030: Started on Oct 22, 2020 at 21:50:50 EDT
+irun
+	-gui
+	-access +rwc
+	-l div.sv
+	tb_div.sv
+file: tb_div.sv
+	module worklib.tb_div:sv
+		errors: 0, warnings: 0
+		Caching library 'worklib' ....... Done
+	Elaborating the design hierarchy:
+		Caching library 'worklib' ....... Done
+  div #(32) DUT (.*);
+              |
+ncelab: *E,CUVMUR (./tb_div.sv,15|14): instance 'tb_div.DUT' of design unit 'div' is unresolved in 'worklib.tb_div:sv'.
+irun: *E,ELBERR: Error during elaboration (status 1), exiting.
+TOOL:	irun(64)	15.20-s030: Exiting on Oct 22, 2020 at 21:50:50 EDT  (total: 00:00:00)
