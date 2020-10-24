26d2c7f1b89daa8ef85bbdff9f876b52e656b855
Project32 SOC <socet32@asicfab.ecn.purdue.edu>
2020-10-23 21:19:40 -0400

    test case change

--- source_code/risc_mgmt/extensions/rv32m/tb_div.sv
+++ source_code/risc_mgmt/extensions/rv32m/tb_div.sv
@@ -68,7 +68,7 @@ nRST = 1'b1;
 @(posedge CLK);
 
 //       80/2         unsigned = 40 .... 0
-test_div(32'h80,32'h2,1'h0,32'h40, 32'h0);
+test_div(32'd80,32'd2,1'd0,32'd40, 32'h0);
 
 test_div(32'hFFFFFFF7,32'h3,1'h1,32'hFFFFFFFD, 32'h0);//-9 / 3 = -3...0
 
