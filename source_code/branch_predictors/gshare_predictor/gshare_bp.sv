5aa1636e4a6edf3d37d7d09898913c5606e9db46
Xinlue Liu <liu2141@purdue.edu>
2021-03-14 22:24:03 -0400

    update gshare_bp

---
+++ source_code/branch_predictors/gshare_predictor/gshare_bp.sv
@@ -0,0 +1,13 @@
+ude "predictor_pipeline_if.vh"
+
+module gshare_bp (
+        input logic CLK, nRST,
+        predictor_pipeline_if.predictor predict_if,
+        predictor_pipeline_if.pht_prediction pht_if,
+        predictor_pipeline_if.btb_prediction btb_if
+);
+
+    assign predict_if.predict_taken = pht_if.taken && btb_if.hit;
+    assign predict_if.target_addr = predict_taken ? btb_if.address : predict_if.current_pc + 4;
+        
+endmodule
---
+++ source_code/branch_predictors/gshare_predictor/pht.sv
@@ -0,0 +1,9 @@
+include "predictor_pipeline_if.vh"
+
+module pht (
+        input logic CLK, nRST, 
+        predictor_pipeline_if.predictor predict_if,
+        predictor_pipeline_if.pht_prediction pht_if
+);
+        
+endmodule
