aafa4a6390afccb83377107eddcd95926a5faa97
Hadi Ahmed <ahmed138@purdue.edu>
2022-10-30 23:19:04 -0400

    Fixed PMA test

--- verification/pma-tests/pma.c
+++ verification/pma-tests/pma.c
@@ -9,6 +9,12 @@ volatile uint32_t *pma_rom_addr = (uint32_t*) PMA_ROM_ADDR;
 volatile uint32_t *pma_ram_addr = (uint32_t*) PMA_RAM_ADDR;
 
 void __attribute__((interrupt)) __attribute__((aligned(4))) handler() {
+    // In a real program, a fault should be handled differently
+    uint32_t mepc_value;
+    asm volatile("csrr %0, mepc" : "=r"(mepc_value));
+    mepc_value += 4;
+    asm volatile("csrw mepc, %0" : : "r"(mepc_value));
+
     print("PMA Checker failed\n");
     flag = 2;
 }
@@ -21,7 +27,7 @@ int main() {
     asm volatile("csrw mtvec, %0" : : "r" (mtvec_value));
 
     // This should fail
-    //*pma_rom_addr = 0xDEADBEEF;
+    *pma_rom_addr = 0xDEADBEEF;
 
     // This should succeed
     *pma_ram_addr = 0xDEADBEEF;
