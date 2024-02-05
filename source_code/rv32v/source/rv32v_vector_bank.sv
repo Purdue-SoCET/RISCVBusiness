1b59dd91028f970d7ca145196a8c8f99e180ba32
socet137 <socet137@asicfab.ecn.purdue.edu>
2024-02-04 22:31:44 -0500

    start some blocks in the execute stage

---
+++ source_code/rv32v/source/rv32v_vector_bank.sv
@@ -0,0 +1,62 @@
+/*
+*   Copyright 2024 Purdue University
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*
+*
+*   Filename:     rv32v_vector_bank
+*
+*   Created by:   Fahad Aloufi
+*   Email:        faloufi@purdue.edu
+*   Date Created: 2/3/2024
+*   Description:  vector reg file bank
+*/
+
+import rv32i_types_pkg::*;
+
+module rv32v_vector_bank
+(
+    input logic CLK, nRST, 
+
+    input logic[4:0] vs1, vs2, vw,   
+    input word_t vwdata, 
+    input logic[3:0] byte_wen, 
+
+    output word_t vdat1, vdat2
+);
+
+word_t[31:0]  bank_reg_file; 
+
+assign vdat1 = bank_reg_file[vs1];
+assign vdat2 = bank_reg_file[vs2];
+
+always_ff @(posedge CLK, negedge nRST) begin
+    if(~nRST) begin
+        bank_reg_file = '{{32{32'b0}}}; 
+    end
+    else begin
+        if(byte_wen[3])
+            bank_reg_file[vw][31:24] = vwdat[31:24];
+
+        if(byte_wen[2])
+            bank_reg_file[vw][23:16] = vwdat[23:16];
+
+        if(byte_wen[1])
+            bank_reg_file[vw][15:8] = vwdat[15:8];
+
+        if(byte_wen[0])
+            bank_reg_file[vw][7:0] = vwdat[7:0]; 
+    end
+end
+
+endmodule
---
+++ source_code/rv32v/source/rv32v_write_xbar.sv
@@ -0,0 +1,73 @@
+/*
+*   Copyright 2024 Purdue University
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*
+*
+*   Filename:     rv32v_write_xbar.sv
+*
+*   Created by:   Fahad Aloufi
+*   Email:        faloufi@purdue.edu
+*   Date Created: 2/3/2024
+*   Description:  Mappings from vector lane computation to vector bank
+*/
+
+import rv32i_types_pkg::*;
+
+module rv32v_write_xbar
+#(
+    // parameter of which bank is receiving Writeback signals generated in this module 
+    BANK_NUM = 0 
+)
+(
+    input word_t [3:0] lane_dat, 
+    input logic[3:0] lane_wen, 
+    input logic[1:0] eew,
+    input logic[1:0] bank_offset, 
+
+    output word_t vwdat,
+    output logic[3:0] byte_wen
+);
+
+typdef enum logic[1:0] {BYTE_4 = 2'd0, BYTE_2 = 2'd1, BYTE_1 = 2'd2} EEW_TYPE; // put this in a package
+
+always_comb begin
+    //default
+    vwdata = '0; 
+    byte_wen = '0; 
+
+    if(EEW_TYPE'(eew) == BYTE_4) begin
+        vwdata = lane_dat[BANK_NUM];
+        byte_wen = lane_wen[BANK_NUM] ? 4'b1111 : 4'b0000;  
+    end
+    else if(EEW_TYPE'(eew) == BYTE_2) begin
+        if(BANK_NUM == bank_offset) begin
+            vwdata = {lane_dat[1][31:16], lane_dat[0][15:0]}; 
+            byte_wen = { { 2{lane_wen[1]} }, { 2{lane_wen[0]} } }; 
+        end 
+        else if(BANK_NUM == (bank_offset + 1)) begin
+            vwdata = {lane_dat[3][31:16], lane_dat[2][15:0]}; 
+            byte_wen = { { 2{lane_wen[3]} }, { 2{lane_wen[2]} } }; 
+        end
+    end 
+    else begin
+        if(BANK_NUM == bank_offset) begin
+            vwdata = {lane_dat[3][31:24], lane_dat[2][23:16], lane_dat[1][15:8], lane_dat[0][7:0]}; 
+            byte_wen = lane_wen; 
+        end 
+    end
+end
+
+
+
+endmodule
