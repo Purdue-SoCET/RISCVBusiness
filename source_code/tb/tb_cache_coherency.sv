7fc47d2fa8d685cd0d4bac8e13ae8e860eb06110
sahin01 <sahin0@purdue.edu>
2024-02-14 14:12:15 -0500

    Wrapper file and testbench separated for coherency-cache

--- source_code/tb/tb_cache_coherency.sv
+++ source_code/tb/tb_cache_coherency.sv
@@ -1,11 +1,9 @@
-// Written by: Malcolm McClymont
-// Last updated: 2/13/24
+// Written by: Burkay Sahin
+// Last updated: 2/14/24
 //
 // Module Summary:
 //   A testbench that combines an L1 cache and a coherency unit.
 //   Designed to test bus requests and responses
-
-
 
 `include "generic_bus_if.vh"
 `include "cache_control_if.vh"
@@ -15,51 +13,39 @@
 `timescale 1ns/100ps
 
 module tb_cache_coherency();
-    
-    logic CLK, nRST;
+    parameter PERIOD = 10;
+    logic CLK = 0, nRST;
+    // clock
+    always #(PERIOD/2) CLK++; 
+
     generic_bus_if.generic_bus proc_gen_bus_if(); //Processor to cache
     cache_control_if.dcache cc_if(); //Processor to cache
     generic_bus_if.cpu mem_gen_bus_if(); //Cache to coherency unit
     cache_coherence_if d_cache_coherency_if (); //Cache to coherency unit
     bus_ctrl_if bus_ctrl_if(); //Coherency unit to bus
 
-    l1_cache #(
-        .CACHE_SIZE(1024),
-        .BLOCK_SIZE(2),
-        .ASSOC(1),
-        .NONCACHE_START_ADDR(32'hF000_0000),
-        .CPUS(1)
-    )
-    dcache (
-        .CLK(CLK),
-        .nRST(nRST),
-        .mem_gen_bus_if(mem_gen_bus_if),
-        .proc_gen_bus_if(proc_gen_bus_if),
-        .flush(cc_if.dcache_flush),
-        .clear(cc_if.dcache_clear),
-        .reserve(cc_if.dcache_reserve),
-        .exclusive(cc_if.dcache_exclusive),
-        .flush_done(cc_if.dflush_done),
-        .clear_done(cc_if.dclear_done),
-        .ccif(d_cache_coherency_if)
-    );
-
-
-
-
-
-    coherency_unit #(
-        .CPUID(1)
-    ) d_coherence_unit (
-        .CLK(CLK),
-        .nRST(nRST),
-        .ccif(d_cache_coherency_if),
-        .bcif(bus_ctrl_if),
-        .gbif(mem_gen_bus_if)
-    );
-
-
+  // test program
+  test PROG (CLK, nRST, proc_gen_bus_if, cc_if, mem_gen_bus_if, d_cache_coherency_if, bus_ctrl_if);
+  // DUT
+  cache_coh_wrapper DUT(CLK, nRST, proc_gen_bus_if, cc_if, mem_gen_bus_if, d_cache_coherency_if, bus_ctrl_if);
 
 
 endmodule
 
+
+program test
+(
+    input CLK, nRST,
+    generic_bus_if.generic_bus gbif, //Processor to cache
+    cache_control_if.dcache ccif, //Processor to cache
+    generic_bus_if.cpu mbif, //Cache to coherency unit
+    cache_coherence_if dcif, //Cache to coherency unit
+    bus_ctrl_if bcif  //Coherency unit to bus
+);
+integer tb_test_num;
+string tb_test_case;
+
+initial begin
+end
+
+endprogram
