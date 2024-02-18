e5a5ae8329b509482dbcbe04dcefd5b3e173b6e4
Rohan Gangaraju <gangaraju.rohan@gmail.com>
2024-02-18 12:49:04 -0500

    done

--- verification/multithread-tests/c-tests/sort_help.c
+++
@@ -1,5 +0,0 @@
-#include "sort_help.h"
-
-void add(volatile int* num) {
-    (*num)++;
-}
--- verification/multithread-tests/c-tests/sort_help.h
+++
@@ -1,7 +0,0 @@
-#ifndef __SORT_HELP_H__
-#define __SORT_HELP_H__
-
-    #include <stdint.h>
-    void add(volatile int* num);
-
-#endif
