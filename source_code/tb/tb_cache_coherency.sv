20445387ace115b994a29929b684014adbb8831d
sahin01 <sahin0@purdue.edu>
2024-02-17 23:01:03 -0500

    Initial test case for cache coherency

--- source_code/tb/tb_cache_coherency.sv
+++ source_code/tb/tb_cache_coherency.sv
@@ -46,6 +46,47 @@ integer tb_test_num;
 string tb_test_case;
 
 initial begin
+    //Test case 1: Transition I -> E
+    tb_test_num = 1;
+    tb_test_case = "Transition I -> E";
+
+    //Reset to isolate each test case
+    nRST = 1'b0;
+    @(posedge CLK);
+    nRST = 1'b1;
+    @(posedge CLK);
+
+    gbif.addr = 32'h33311133; 
+    gbif.ren = 1'b1; //Coherency unit should go to READ_REQ state
+    wait (dcif.dwait == 1'b0); // Wait for dwait to go low
+
+    // Cache sets dREN[I] high
+    wait(ccif.dREN = 1'b1);
+
+    // Bus transitions IDLE -> GRANT_R
+    bcif.ccsnoopaddr = gbif.daddr;
+    bcif.ccwait[1] = 1'b1; // Assert ccwait for all non-requester CPUs
+    #(10); //Some time to pass for snooping
+
+    wait(dcif.ccsnoopdone[1] = 1'b1); // All non-requester CPUs raise ccsnoopdone
+    wait(dcif.ccsnoophit[1] = 1'b0); // None raise ccsnoophit
+
+    // Transition SNOOP_R -> READ_L2
+    bcif.l2REN = 1; // Bus sets l2REN high
+    wait(ccif.dload = bcif.l2load); // Cache loads data from L2
+
+    // Transition BUS_TO_CACHE
+    wait(dcif.dwait = 0); // Bus sets dwait low
+    wait(dcif.ccexclusive = 1); // Bus sets ccexclusive high
+
+    // Transition back to IDLE, de-assert signals
+    bcif.ccwait = 0; // De-assert ccwait signals
+    @(posedge CLK); // Wait for the changes to propagate
+
+    //Look at the coherency unit outputs
+    assert(dcif.cache_transfer == EXCLUSIVE) $display("Cache transfer state correct");
+    else $error("Cache transfer state incorrect")
+
 end
 
 endprogram
