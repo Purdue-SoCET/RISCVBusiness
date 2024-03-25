13f40cf439139fba4be5f8a38b3363bf3d240c25
Devin Singh <drsingh2518@icloud.com>
2024-03-24 22:37:43 -0400

    cpu_tracker: parameterize by number of harts

--- source_code/standard_core/top_core_no_memory.sv
+++ source_code/standard_core/top_core_no_memory.sv
@@ -43,7 +43,7 @@ module top_core #(
         get_x28 = CORE.pipeline.execute_stage_i.g_rfile_select.rf.registers[28];
     endfunction
 
-    bind stage3_mem_stage cpu_tracker cpu_track1 (
+    bind stage3_mem_stage cpu_tracker #(.NUM_HARTS(1)) cpu_track1 (
         .CLK(CLK),
         .wb_stall(wb_stall),
         .instr(ex_mem_if.ex_mem_reg.instr),
