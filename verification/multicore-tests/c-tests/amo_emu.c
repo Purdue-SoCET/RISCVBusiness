a84ca44d6e586fcd7a750910273e401cd88c908d
Devin Singh <drsingh2518@icloud.com>
2024-03-27 23:13:05 -0400

    test/multicore: don't clobber the sp on an exception

--- verification/multicore-tests/c-tests/amo_emu.c
+++ verification/multicore-tests/c-tests/amo_emu.c
@@ -123,9 +123,7 @@ static uint32_t get_mepc() {
 }
 
 __attribute__((noreturn, aligned(4))) void exception_handler() {
-    __asm__ volatile("lw ra, 12(sp)\n"
-                     "addi sp, sp, 16\n" // Weird hack to undo function prologue
-                     "addi sp, sp, -128\n"
+    __asm__ volatile("addi sp, sp, -128\n"
                      "sw x31, 124(sp)\n"
                      "sw x30, 120(sp)\n"
                      "sw x29, 116(sp)\n"
@@ -155,7 +153,6 @@ __attribute__((noreturn, aligned(4))) void exception_handler() {
                      "sw x5, 20(sp)\n"
                      "sw x4, 16(sp)\n"
                      "sw x3, 12(sp)\n"
-                     "sw x2, 8(sp)\n"
                      "sw x1, 4(sp)\n");
     uint32_t *regs;
     __asm__ volatile("mv %0, sp" : "=r"(regs)::);
@@ -179,7 +176,6 @@ __attribute__((noreturn, aligned(4))) void exception_handler() {
         funct3 = (instr & 0x00007000) >> 12;
         funct7 = (instr & 0xFE000000) >> 25;
 
-        // TODO: support SWAP, MIN[U], MAX[U] emulation
         if (opcode == AMO_OPCODE && funct3 == 0x2) {
             switch (funct7 >> 2) {
             case AMO_ADD_FUNCT5:
@@ -243,7 +239,6 @@ __attribute__((noreturn, aligned(4))) void exception_handler() {
                          "lw x5, 20(sp)\n"
                          "lw x4, 16(sp)\n"
                          "lw x3, 12(sp)\n"
-                         "lw x2, 8(sp)\n"
                          "lw x1, 4(sp)\n"
                          "addi sp, sp, 128\n"
                          "mret");
