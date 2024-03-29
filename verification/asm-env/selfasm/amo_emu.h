16a6a588633187d919c6bdf481ca2fc44cd5d873
Devin Singh <drsingh2518@icloud.com>
2024-03-29 11:01:47 -0400

    selfasm: properly set up stack for multiple harts

--- verification/asm-env/selfasm/amo_emu.h
+++ verification/asm-env/selfasm/amo_emu.h
@@ -4,7 +4,10 @@
 #define WANT_AMO_EMU_SUPPORT                                                                       \
     .globl __stack_top;                                                                            \
     .globl exception_handler;                                                                      \
+    csrr s0, mhartid;                                                                              \
     la sp, __stack_top;                                                                            \
+    slli t0, s0, 11;                                                                               \
+    add sp, sp, t0;                                                                                \
     la t0, exception_handler;                                                                      \
     csrw mtvec, t0;
 
