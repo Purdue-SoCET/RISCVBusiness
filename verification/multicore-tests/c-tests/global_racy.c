b16f477f1996cd4f340c438402794e306713939a
Devin Singh <drsingh2518@icloud.com>
2024-04-03 23:44:41 -0400

    multicore-tests: fix global tests and make hart1_done volatile

--- verification/multicore-tests/c-tests/global_racy.c
+++ verification/multicore-tests/c-tests/global_racy.c
@@ -1,18 +1,22 @@
 #include "utility.h"
 
+#define N 35
+
 int global = 0;
 
 void hart0_main() {
-    for (int i = 0; i < 10; i++) {
-        global++;
+    volatile int *ptr = &global;
+    for (int i = 0; i < N; i++) {
+        (*ptr)++;
     }
     while (hart1_done == 0) {}
-    flag = global == 20;
+    flag = global == (N * 2);
 }
 
 void hart1_main() {
-    for (int i = 0; i < 10; i++) {
-        global++;
+    volatile int *ptr = &global;
+    for (int i = 0; i < N; i++) {
+        (*ptr)++;
     }
     hart1_done = 1;
 }
