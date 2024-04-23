f802dfc44233ebd5340e6f61a992c7ea3c7159f2
Pranav Srisankar <psrisank@purdue.edu>
2024-04-23 15:51:53 -0400

    attempted fix

--- source_code/caches/l1/run_l1_test.sh
+++ source_code/caches/l1/run_l1_test.sh
@@ -1,2 +1,2 @@
-vlog -sv -work work l1_cache_tb.sv l1_cache.sv ../sram/sram.sv +incdir+/home/asicfab/a/socet65/fgmt/RISCVBusiness/source_code/include
+vlog -sv -work work l1_cache_tb.sv l1_cache.sv ../sram/sram.sv +incdir+../../include
 vsim -voptargs="+acc" work.l1_cache_tb -do "do wave.do; run -all"
