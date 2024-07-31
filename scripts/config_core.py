ae74501520f17e26a229e79f5ebed1cac967cda1
wrcunnin <105618606+wrcunnin@users.noreply.github.com>
2024-07-31 14:18:35 -0400

    PMP Granularity Revision (#38)
    Add configurable granularity for PMP registers
    
    This parameterizes the minimum granularity for the PMP implementation to allow for area savings (in exchange for coarser-grained regions). Setting the granularity (G) higher than 4B will remove the NA4 option.

--- scripts/config_core.py
+++ scripts/config_core.py
@@ -25,13 +25,52 @@
 import yaml
 import argparse 
 import sys
+from math import log2
 
 VH_FILE = 'source_code/include/component_selection_defines.vh'
 C_FILE  = 'verification/c-firmware/custom_instruction_calls.h'
 
+# If running PMP tests, the macro G in verification/pmp-tests/utility.h will need to 
+# be updated to the corresponding value found in PMP_MINIMUM_GRANULARITY.
+PMP_MINIMUM_GRANULARITY = \
+  {
+    '4'    :  0,
+    '8'    :  1,
+    '16'   :  2,
+    '32'   :  3,
+    '64'   :  4,
+    '128'  :  5,
+    '256'  :  6,
+    '512'  :  7,
+    '1K'   :  8,
+    '2K'   :  9,
+    '4K'   : 10,
+    '8K'   : 11,
+    '16K'  : 12,
+    '32K'  : 13,
+    '64K'  : 14,
+    '128K' : 15,
+    '256K' : 16,
+    '512K' : 17,
+    '1M'   : 18,
+    '2M'   : 19,
+    '4M'   : 20,
+    '8M'   : 21,
+    '16M'  : 22,
+    '32M'  : 23,
+    '64M'  : 24,
+    '128M' : 25,
+    '256M' : 26,
+    '512M' : 27,
+    '1G'   : 28,
+    '2G'   : 29,
+    '4G'   : 30,
+  }
+
 ISA_PARAMS = \
   {
-    'xlen' : [32]
+    'xlen' : [32],
+    'pmp_minimum_granularity' : list(PMP_MINIMUM_GRANULARITY.keys())
   }
 
 UARCH_PARAMS = \
@@ -135,9 +174,11 @@ def create_include(config):
         sys.exit(err)
       else:
         line = 'localparam '
-        # xlen will be an integer in include file, so no quotes needed
+        # xlen & pmp_minimum_granularity will be an integer in include file, so no quotes needed
         if 'xlen' == isa_param:
           line += isa_param.upper() + ' = ' + str(isa_params[isa_param])
+        elif 'pmp_minimum_granularity' == isa_param:
+          line += isa_param.upper() + ' = ' + str(PMP_MINIMUM_GRANULARITY[isa_params[isa_param]])
         else:
           line += isa_param.upper() + ' = "' + isa_params[isa_param] + '"'
         line += ';\n'
--- source_code/privs/priv_1_12/priv_1_12_pmp.sv
+++ source_code/privs/priv_1_12/priv_1_12_pmp.sv
@@ -35,6 +35,8 @@ module priv_1_12_pmp (
   import machine_mode_types_1_12_pkg::*;
   import rv32i_types_pkg::*;
 
+  localparam NAPOT_ADDR_BITS = PMP_MINIMUM_GRANULARITY == 0 ? 1 : PMP_MINIMUM_GRANULARITY;
+
   pmpcfg_t [3:0] pmp_cfg_regs, nxt_pmp_cfg;
   pmpaddr_t [15:0] pmp_addr_regs, nxt_pmp_addr;
   pmpcfg_t new_cfg;
@@ -56,6 +58,7 @@ module priv_1_12_pmp (
 
   assign pmp_cfg_addr_add_one_reg = (priv_ext_if.csr_addr[3:0] + 1) >> 2;   // exists because TOR is weird
   assign pmp_cfg_addr_add_one_cfg = (priv_ext_if.csr_addr[3:0] + 1) & 2'h3; // exists because TOR is weird
+
   always_comb begin
     nxt_pmp_addr = pmp_addr_regs;
     nxt_pmp_cfg = pmp_cfg_regs;
@@ -68,6 +71,7 @@ module priv_1_12_pmp (
           new_cfg[1].reserved = '0;
           new_cfg[2].reserved = '0;
           new_cfg[3].reserved = '0;
+
           // WARL check (R/W)
           if (new_cfg[0].R == 1'b0 && new_cfg[0].W == 1'b1) begin
             new_cfg[0].W = 1'b0;
@@ -80,6 +84,18 @@ module priv_1_12_pmp (
           end
           if (new_cfg[3].R == 1'b0 && new_cfg[3].W == 1'b1) begin
             new_cfg[3].W = 1'b0;
+          end
+
+          // Remove NA4 with granularities > 0
+          if (PMP_MINIMUM_GRANULARITY != 0) begin
+            if (new_cfg[0].A == NA4)
+              new_cfg[0].A = NAPOT;
+            if (new_cfg[1].A == NA4)
+              new_cfg[1].A = NAPOT;
+            if (new_cfg[2].A == NA4)
+              new_cfg[2].A = NAPOT;
+            if (new_cfg[3].A == NA4)
+              new_cfg[3].A = NAPOT;
           end
 
           // Make sure we cannot write to locked CSRs
@@ -106,10 +122,14 @@ module priv_1_12_pmp (
             //    pmpcfg(i) might be TOR, which means it uses both pmpaddr(i) and pmpaddr(i-1)
             //    pg 60 of the v1.12 specification for more info
             if (priv_ext_if.csr_addr[3:0] != 15) begin // 15 is the last valid register, can't check the one above it
-              if (pmp_cfg_regs[pmp_cfg_addr_add_one_reg][pmp_cfg_addr_add_one_cfg].A != TOR) begin // If not TOR, everything is good
-                nxt_pmp_addr[priv_ext_if.csr_addr[3:0]] = priv_ext_if.value_in;
-              end else if (~pmp_cfg_regs[pmp_cfg_addr_add_one_reg][pmp_cfg_addr_add_one_cfg].L) begin // It was TOR, and is not locked
-                nxt_pmp_addr[priv_ext_if.csr_addr[3:0]] = priv_ext_if.value_in;
+              if ((pmp_cfg_regs[pmp_cfg_addr_add_one_reg][pmp_cfg_addr_add_one_cfg].A != TOR) || // If not TOR, everything is good
+                  (~pmp_cfg_regs[pmp_cfg_addr_add_one_reg][pmp_cfg_addr_add_one_cfg].L)) begin   // It was TOR, and is not locked
+                // If NA4 is allowed or if lower bits are all 1's
+                if (PMP_MINIMUM_GRANULARITY == 0 || (&priv_ext_if.value_in[(NAPOT_ADDR_BITS-1):0]))
+                  nxt_pmp_addr[priv_ext_if.csr_addr[3:0]] = priv_ext_if.value_in; // keep address as is
+                else begin
+                  nxt_pmp_addr[priv_ext_if.csr_addr[3:0]] = (priv_ext_if.value_in & ~((32'b1 << NAPOT_ADDR_BITS) - 1)) | ((32'b1 << (NAPOT_ADDR_BITS - 1)) - 1); // change to lowest granularity
+              end
               end
             end
           end
