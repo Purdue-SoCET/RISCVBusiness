5194e506184dd3e024a4bfa533db675ac0adde71
socet209 <socet209@asicfab.ecn.purdue.edu>
2024-10-25 14:00:21 -0400

    Data Monitor, rest in progress

---
+++ verification/uvm/multicore/data-agent/data_driver.svh
@@ -0,0 +1,6 @@
+`ifndef DATA_DRIVER_SVH
+`define DATA_DRIVER_SVH
+
+import uvm_pkg::*;
+
+`include "
---
+++ verification/uvm/multicore/data-agent/data_monitor.svh
@@ -0,0 +1,79 @@
+`ifndef DATA_MONITOR_SVH
+`define DATA_MONITOR_SVH
+
+import uvm_pkg::*;
+`include 'uvm_macros.svh'
+
+// --- Includes --- //
+`include "data_transaction.svh"
+`include "multicore_if.sv"
+// TODO: note new bus changes in if
+
+// --- Data Transaction --- //
+class data_monitor extends uvm_monitor;
+	`uvm_component_utils(data_monitor)
+
+	// --- Virtual Interface --- //
+	virtual multicore_if mcvif;
+
+	// --- Transaction --- //
+	data_transaction data_trans;
+
+	// --- Create Analysis Port --- //
+	uvm_analysis_port#(data_transaction) data_ap;
+
+	// --- Constructor --- //
+	function new(string name = "data_monitor", uvm_component parent = null);
+		super.new(name, parent);
+		`uvm_info("DATA_MONITOR", "Inside Constructor", UVM_HIGH)
+	endfunction
+
+	// --- Build Phase --- //
+	virtual function void build_phase (uvm_phase phase);
+		super.build_phase(phase);
+		`uvm_info("DATA_MONITOR", "Build Phase", UVM_HIGH)
+		
+		// --- Build Monitor Port --- //
+		data_ap = new("data_ap", this);
+		
+		if(!uvm_config_db #(virtual multicore_if)::get(this, "", "mcif", mcif)) begin
+			`uvm_fatal("DATA_MONITOR", "No virtual interface for multicore_if)
+		end
+	endfunction : build_phase
+
+	// --- Run Phase --- //
+	virtual task run_phase(uvm_phase phase);
+		super.run_phase(phase);
+		`uvm_info("DATA_MONITOR", "Run Phase", UVM_HIGH)
+
+		// --- Create Data Transaction --- //		
+		data_trans = data_transaction::type_id::create("data_trans");
+
+		// --- Run Phase --- //
+		forever begin
+
+			wait(mcvif.NRST);
+
+			// TODO: fill in sequence item
+
+			// --- Input Sample --- //
+			@(posedge mcvif.CLK);			
+			
+			// data_trans.SIG1 = mcvif.SIG1;
+			// data_trans.SIG2 = mcvif.SIG2;
+
+			// --- Output Sample --- //
+
+			@(posedge mcvif.CLK);			
+
+			// data_trans.SIG3 = mcvif.SIG3;
+			// data_trans.SIG4 = mcvif.SIG4;
+		
+			// --- Send to Scoreboard --- //
+			data_ap.write(data_trans);
+	
+		end
+	endtask : run_phase
+endclass : data_monitor
+
+`endif
