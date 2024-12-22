50eb000bd551d2c2873af67261c5fbc48914404f
Devin Singh <drsingh2518@icloud.com>
2024-12-22 08:33:22 -0500

    l1_cache: stub out coherence unit in l1_cache

--- source_code/tb/tb_cache_coherence_stress/build.sh
+++ source_code/tb/tb_cache_coherence_stress/build.sh
@@ -4,6 +4,6 @@ set -e
 
 fusesoc --cores-root ../../. --cores-root . run --setup --build --build-root out --target sim socet:tb:cache_coherence_stress
 echo "Built testbench!"
-./out/sim-verilator/Vcache_stress_wrapper $1
+./out/socet_tb_cache_coherence_stress_0.1.0/sim-verilator/Vcache_stress_wrapper $1
 echo "Ran testbench!"
 diff goldensim.dump memsim.dump
