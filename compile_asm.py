151deb53e27ce55ac43320860ce5e73083ae6bed
Cole Nelson <nelso345@purdue.edu>
2025-05-04 18:50:37 -0400

    Cleanup: Remove unnecessary scripts

--- compile_asm.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-from run_tests import compile_asm
-
-if __name__ == '__main__':
-    compile_asm(sys.argv[1])
--- compile_c.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-from run_tests import compile_c
-
-if __name__ == '__main__':
-    compile_c(sys.argv[1])
-
