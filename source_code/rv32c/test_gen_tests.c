acb1f66b663f7c62acd7fd08ff5672032e78066b
Cole Nelson <nelso345@purdue.edu>
2025-05-20 22:31:41 -0400

    rv32c: Finish test generation program

--- source_code/rv32c/test_gen_tests.c
+++ source_code/rv32c/test_gen_tests.c
@@ -27,6 +27,32 @@ const struct {
     // C.SW cases - store word instructions
     // Format: c.sw rs2', offset(rs1')
     {0xddb4, 0x06d5ac23},  // C.SW: sw a3, 120(a1)
+    {0x073a, 0x00e71713}, // C.SLLI
+    {0x3462, 0x03813407}, // C.FLDSP fs0, 56
+    {0x4752, 0x01412703}, // C.LWSP a4, 20
+    {0x8b82, 0x000b8067}, // C.JR s7
+    {0x8e26, 0x00900e33}, // C.MV t3, s1
+    {0x9002, 0x00100073}, // C.EBREAK
+    {0x9182, 0x000180e7}, // C.JALR 
+    {0x931e, 0x00700333}, // C.ADD t1, t2
+    {0xac26, 0x00913c27}, // C.FSDSP fs1, 24
+    {0xd22e, 0x02b12223}, // C.SWSP a1, 36
+    // C1 quadrant
+    {0x1, 0x00000013}, // c.nop
+    {0x1f7d, 0xffff0f13}, // c.addi x30, -1
+    {0x5735, 0xfed00713}, // c.li a4, -19
+    {0x7111, 0xf0010113}, // c.addi16sp -256
+    {0x6229, 0x0000a237}, // c.lui
+    {0x80cd, 0x0134d493}, // c.srli
+    {0x84cd, 0x4134d493}, // c.srai
+    {0x9885, 0xfe14f493}, // c.andi s1, 0x61
+    {0x8c05, 0x40940433}, // c.sub s0, s1 
+    {0x8da9, 0x00a5c5b3}, // c.xor a1, a0
+    {0x8e55, 0x00d66633}, // c.or a2, a3
+    {0x8f7d, 0x00f77733}, // c.and a4, a5
+    {0xbfed, 0xffbff06f},
+    {0xd001, 0xf00400e3}, // c.beqz s0, -256
+    {0xecfd, 0x0e049f63}, // c.bnez s1, 254
 #ifdef RV64C_MODE
     // RV64C-specific instructions
 
@@ -36,7 +62,11 @@ const struct {
     // C.SD cases (RV32C only) - store single-precision floating point
     // Format: c.fsw rs2', offset(rs1')
     {0xe004, 0x00943023},  // C.SD: sd s1, 0(s0)
-    
+    {0x6632, 0x10813603}, // C.LDSP a2, 264(sp)
+    {0xf4ca, 0x07213423}, // C.SDSP s2, 104
+    {0x3fc5, 0xff1f8f9b}, // c.addiw t6, -15
+    {0x9fa9, 0x00a787bb}, // c.addw a5, a0
+    {0x9d81, 0x408585bb}, // c.subw a1, a1, s0
 #else
     // RV32C-specific instructions
 
@@ -46,7 +76,9 @@ const struct {
     // C.FSW cases (RV32C only) - store single-precision floating point
     // Format: c.fsw rs2', offset(rs1')
     {0xe004, 0x00942027},  // C.FSW: fsw fs1, 0(s0)
-
+    {0x6632, 0x00c12607}, // C.FLWSP fa2, 12
+    {0xf4ca, 0x07212427}, // C.FSWSP fs2, 104
+    {0x3fc5, 0xff1ff0ef}, // c.jal -16
 #endif
 };
 
