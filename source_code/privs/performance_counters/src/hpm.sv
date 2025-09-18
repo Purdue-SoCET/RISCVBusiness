1db12f57bbec252fc863cb0b5ad3f58927e9b082
DanielWunderlich <wunderld@purdue.edu>
2025-09-18 00:23:24 -0400

    make tb for hpm.sv

--- source_code/privs/performance_counters/src/hpm.sv
+++ source_code/privs/performance_counters/src/hpm.sv
@@ -1,21 +1,5 @@
+`timescale 1ns / 10ps
 `include "priv_ext_if.vh"
-
-/*modport ext (
-    output invalid_csr, ack, value_out,
-    input csr_addr, value_in, csr_active
-);*/
-
-/*module counter64bit (
-input logic clk,
-input logic n_rst,
-input logic enable, //counter enable
-input logic [31:0] overwrite_val, //overwrite value, can only overwrite 32 bits of the counter at a time
-input logic [1:0] csr_active_sel, // 0 --> not overwriting; 1 --> write to lower 32 bits ; 2 --> write to upper 32 bits
-input logic clear, //clear all 64 bits of counter
-
-output logic [63:0] count_out,
-output logic rollover_flag
-);*/
 
 module hpm #(
    parameter NUM_PEFORMANCE_COUNTERS = 2 // ASSUME DEFAULT OF 2 FOR NOW 
@@ -23,8 +7,7 @@ module hpm #(
     input logic clk,
     input logic n_rst,
 
-    logic enable1, // Temporary enable signals ==> these will be pulled from CPU core
-    logic enable2,
+    input logic enable_array [NUM_PEFORMANCE_COUNTERS - 1 : 0],
 
     priv_ext_if.ext priv
 );
@@ -39,52 +22,81 @@ localparam ADDRLOWERCOUNTER2 = 12'hC01; // ADDRESS TO ACCESS LOWER 32 BITS OF SE
 localparam ADDRUPPERCOUNTER2 = 12'hC81; // ADDRESS TO ACCESS UPPER 32 BITS OF SECOND COUNTER
 
 typedef struct packed {
-    logic [31:0] data;
+    logic [1:0] sel;
     logic [11:0] address;
-} counters_t;
+    logic [31:0] data;
+} counter_sel_map_t;
 
-counters_t counter_overwrite_map [NUM_PEFORMANCE_COUNTERS] = '{
-    '{32'd0, ADDRLOWERCOUNTER1},
-    '{32'd0, ADDRUPPERCOUNTER1},
-    '{32'd0, ADDRLOWERCOUNTER2},
-    '{32'd0, ADDRUPPERCOUNTER2}
-};
+counter_sel_map_t counter_map [NUM_PEFORMANCE_COUNTERS];
+counter_sel_map_t next_counter_map [NUM_PEFORMANCE_COUNTERS];
 
-logic [1:0] csr_active_sel;
+always_ff @(posedge clk, negedge n_rst) begin: COUNTER_MAP_REG
+    if(~n_rst) begin
+        counter_map[0].sel <= NOSEL;
+        counter_map[0].address <= ADDRLOWERCOUNTER1;
+        counter_map[0].data <= 32'd0;
 
-foreach(counter_overwrite_map[i]) begin : WRITE_SEL_COMB
-    if((counter_overwrite_map[i].address == priv.csr_addr) && priv.csr_active) begin // Address is one of ours and we want to overwrite
-        if(priv.csr_addr >= ADDRUPPERCOUNTER1) begin  //Interested in upper bits
-            priv.ack = 1;
-            priv.invalid_csr = 0;
-            csr_active_sel = UPPERSEL;
-            counter_overwrite_map[i].data = value_in;
-        end
-        else begin
-            priv.ack = 1;
-            priv.invalid_csr = 0;
-            csr_active_sel = LOWERSEL;
-            counter_overwrite_map[i].data = value_in;
-        end
+        counter_map[1].sel <= NOSEL;
+        counter_map[1].address <= ADDRLOWERCOUNTER2;
+        counter_map[1].data <= 32'd0;
     end
     else begin
-        priv.ack = 0;
-        priv.invalid_csr = 1;
-        csr_active_sel = NOSEL;
-        counter_overwrite_map[i].data = 0;
+        counter_map <= next_counter_map;
     end
 end
 
-foreach(counter_overwrite_map[i]) begin : READ_SEL_COMB
+always_comb begin : WRITE_SEL_COMB
+    foreach(counter_map[i]) begin
     
+        next_counter_map[i].address = counter_map[i].address;
+
+        if((counter_map[i].address == priv.csr_addr) || ((counter_map[i].address + 128) == priv.csr_addr)  && priv.csr_active) begin // Address is one of ours and we want to overwrite
+            if(priv.csr_addr >= ADDRUPPERCOUNTER1) begin  //Interested in upper bits
+                priv.ack = 1;
+                priv.invalid_csr = 0;
+                next_counter_map[i].sel = UPPERSEL;
+                next_counter_map[i].data = priv.value_in;
+            end
+            else begin
+                priv.ack = 1;
+                priv.invalid_csr = 0;
+                next_counter_map[i].sel = LOWERSEL;
+                next_counter_map[i].data = priv.value_in;
+            end
+        end
+        else begin
+            priv.ack = 0;
+            priv.invalid_csr = 1;
+            next_counter_map[i].sel = NOSEL;
+            next_counter_map[i].data = priv.value_in;
+        end
+    end
 end
 
-genvar i;
+genvar ind;
+logic [63:0] count_out [NUM_PEFORMANCE_COUNTERS-1:0];
 
 generate
-    for(i = 0; i < NUM_PEFORMANCE_COUNTERS; i++) begin
-        counter64bit counter()
+    for(ind = 0; ind < NUM_PEFORMANCE_COUNTERS; ind++) begin
+        counter64bit counter(.clk(clk), .n_rst(n_rst), .enable(enable_array[ind]),
+        .overwrite_val(counter_map[ind].data), .csr_active_sel(counter_map[ind].sel), .clear(),
+        .count_out(count_out[ind]), .rollover_flag());
     end
 endgenerate
+
+always_comb begin : READ_SEL_COMB
+    priv.value_out = 32'd0;
+
+    foreach(counter_map[i]) begin : READ_SEL_COMB
+        if((counter_map[i].address == priv.csr_addr) || ((counter_map[i].address + 128) == priv.csr_addr)) begin
+            if(priv.csr_addr >= ADDRUPPERCOUNTER1) begin
+                priv.value_out = count_out[i][63:32];
+            end
+            else begin
+                priv.value_out = count_out[i][31:0];
+            end
+        end
+    end
+end
 
 endmodule
