028ad33f24c4a37575c865e0363e34358181414a
Cole Nelson <nelso345@purdue.edu>
2025-10-10 17:35:55 -0400

    priv: Fix interrupt timing bug
    This bug occurred when an interrupt was stalled by a pending
    store operation. The PC would update immediately, but priv registers
    only update when the store completes. This pushes the PC redirect
    back to the same timing as mstatus/mepc.

--- verification/interrupts-exceptions/ex_after_int.c
+++ verification/interrupts-exceptions/ex_after_int.c
@@ -3,6 +3,7 @@
 #include "utility.h"
 
 extern volatile int flag;
+extern void done();
 
 volatile uint32_t *mtime        = (uint32_t *)MTIME_ADDR;
 volatile uint32_t *mtimecmp     = (uint32_t *)MTIMECMP_ADDR;
