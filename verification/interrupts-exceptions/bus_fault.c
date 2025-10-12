8db32fbdf70c04bfa5b95a119622f58ed707f488
Cole Nelson <nelso345@purdue.edu>
2025-10-11 23:00:54 -0400

    verification: Cleanup tests
    * Remove bit-rotted test files
    * Centralize custom tests + provide build

--- verification/interrupts-exceptions/bus_fault.c
+++ verification/interrupts-exceptions/bus_fault.c
@@ -1,25 +1,26 @@
 #include <stdint.h>
+#include <stddef.h>
 #include "utility.h"
 
-extern int flag;
+static void *resume_addr = NULL;
 
 void __attribute__((interrupt)) __attribute__((aligned(4))) handler() {
-    uint32_t mepc_value;
-    asm volatile("csrr %0, mepc" : "=r"(mepc_value));
-    mepc_value += 4;
-    asm volatile("csrw mepc, %0" : : "r"(mepc_value));
-    /*
-    asm volatile(
-        "mv t0, zero; csrrs t0, mepc, t0; addi t0, t0, 4; csrw mepc, t0;"
-        :
-        :
-        : "t0"
-    );*/
     print("Made it to handler!\n");
     flag = 1;
+    // Write mepc = resume_addr
+    asm volatile("csrw mepc, %0" : : "r"((uint32_t)resume_addr));
 }
 
 int main() {
+    /**
+     * Note: This uses GCC 'labels as values' feature
+     * to get the address to return from the interrupt.
+     * Can't always assume it will be a 4B instruction,
+     * as it could generate multiple instructions or even
+     * a 2B load.
+     */
+    // get the return address stored in global
+    resume_addr = &&resume_point;
     uint32_t mtvec_value = (uint32_t)handler;
     uint32_t mstatus_value = 0x8;
 
@@ -27,8 +28,15 @@ int main() {
     asm volatile("csrw mtvec, %0" : : "r" (mtvec_value));
 
     print("Read 0x0\n");
-    int x = *(volatile int *)(0x1FFFFFFC);
 
+    // GCC doesn't like us trying to read 0x4
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Warray-bounds"
+    int x = *(volatile int *)(0x4);
+#pragma GCC diagnostic pop
+    (void)x;
+
+resume_point:
     if(flag != 1) {
         print("Failed!\n");
     } else {
--- verification/interrupts-exceptions/ecall.c
+++ verification/interrupts-exceptions/ecall.c
@@ -1,29 +1,16 @@
-
 #include <stdint.h>
 #include "utility.h"
-
-extern volatile int flag;
-
 
 void __attribute__((interrupt)) __attribute__((aligned(4))) handler() {
     uint32_t mepc_value;
     asm volatile("csrr %0, mepc" : "=r"(mepc_value));
     mepc_value += 4;
     asm volatile("csrw mepc, %0" : : "r"(mepc_value));
-    /*
-    asm volatile(
-        "mv t0, zero; csrrs t0, mepc, t0; addi t0, t0, 4; csrw mepc, t0;"
-        :
-        :
-        : "t0"
-    );*/
     print("Made it to handler!\n");
     flag = 1;
 }
 
-
 int main() {
-    
     uint32_t mtvec_value = (uint32_t)handler;
     uint32_t mstatus_value = 0x8;
 
