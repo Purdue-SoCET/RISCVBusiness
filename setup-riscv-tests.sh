ac8cd0304102a7ecfd4a38009ccc5fddf22bd558
William Cunningham <wrcunnin@purdue.edu>
2025-10-30 16:21:00 -0400

    [riscv-tests] updating setup script

--- setup-riscv-tests.sh
+++ setup-riscv-tests.sh
@@ -2,17 +2,13 @@
 
 git submodule update --init --recursive
 pushd riscv-tests
-git apply ../riscv-tests-isa.patch
-pushd env
-git apply ../../riscv-tests-env.patch
+pushd isa
+make clean
 popd
 pushd benchmarks
-git apply ../../riscv-tests-benchmarks.patch
+make clean
 popd
 autoconf
 ./configure
 make $1
-pushd isa
-find . -maxdepth 1 -type f ! -name "Makefile" ! -name "*.*" -exec riscv64-unknown-elf-objcopy -O binary "{}" "{}.bin" \;
-popd
 popd
