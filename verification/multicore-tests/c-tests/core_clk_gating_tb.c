1280c98fd5833f606ed16d3819895bbc4d87c505
aishuanand01 <142924874+aishuanand01@users.noreply.github.com>
2025-12-09 15:57:39 -0500

    removing flag variable

--- verification/multicore-tests/c-tests/core_clk_gating_tb.c
+++ verification/multicore-tests/c-tests/core_clk_gating_tb.c
@@ -12,23 +12,22 @@
 void wfi(void) {
     __asm__ volatile("wfi");
 }
+
 void set_msip(void) {
     MSIP = 1;
 }
+
 void clear_msip(void) {
     MSIP = 0;
 }
 
 //----SHARED-----
-//handshake flags (volitile cause they might change between cores)
-extern volatile uint32_t hart1_ready_for_initial_sleep = 0;
-extern volatile uint32_t hart1_awake_and_worked       = 0;
-extern volatile uint32_t hart1_sleep_again_request    = 0;
-extern volatile uint32_t hart1_sleeping_again         = 0;
+volatile uint32_t hart1_ready_for_initial_sleep = 0;
+volatile uint32_t hart1_awake_and_worked       = 0;
+volatile uint32_t hart1_sleep_again_request    = 0;
+volatile uint32_t hart1_sleeping_again         = 0;
 
-//provided by runtime
-extern volatile uint32_t flag;
-
+//----WORK FUNC----
 void do_some_work(int id) {
     volatile uint32_t x = 0;
     for (int i = 0; i < 1000; i++) {
@@ -39,66 +38,69 @@ void do_some_work(int id) {
 
 //----HART 0-----
 void hart0_main(void) {
-  print("hart0: starting clock-gating sleep/wake test\n");
-  clear_msip();
+    print("hart0: starting clock-gating sleep/wake test\n");
+    clear_msip();
 
-  print("hart0: waiting for hart1 to enter initial WFI\n");
-  while (!hart1_ready_for_initial_sleep) {
-  }
+    print("hart0: waiting for hart1 to enter initial WFI\n");
+    while (!hart1_ready_for_initial_sleep) {
+        // spin
+    }
 
-  print("hart0: hart1 is sleeping; doing some work while it sleeps\n");
-  do_some_work(0);
+    print("hart0: hart1 is sleeping; doing some work while it sleeps\n");
+    do_some_work(0);
 
-  print("hart0: raising MSIP to wake hart1\n");
-  set_msip();
+    print("hart0: raising MSIP to wake hart1\n");
+    set_msip();
   
-  while (!hart1_awake_and_worked) {
-  }
-  print("hart0: hart1 has woken up and done its work\n");
+    while (!hart1_awake_and_worked) {
+        // spin
+    }
+    print("hart0: hart1 has woken up and done its work\n");
 
-  print("hart0: requesting hart1 to sleep again and clearing MSIP\n");
-  hart1_sleep_again_request = 1;
-  clear_msip();
+    print("hart0: requesting hart1 to sleep again and clearing MSIP\n");
+    hart1_sleep_again_request = 1;
+    clear_msip();
 
-  do_some_work(0);
+    do_some_work(0);
 
-  while (!hart1_sleeping_again) {
-  }
+    while (!hart1_sleeping_again) {
+        // spin
+    }
 
-  flag = 1;
-  print("hart0: test complete, flag set to 1\n");
+    flag = 1;
+    print("hart0: test complete, flag set to 1\n");
 
-  return;
+    return;
 }
 
 //----HART 1-----
-
 void hart1_main(void) {
-  print("hart1: starting, preparing for initial sleep\n");
-  clear_msip();
+    print("hart1: starting, preparing for initial sleep\n");
+    clear_msip();
 
-  hart1_ready_for_initial_sleep = 1;
+    hart1_ready_for_initial_sleep = 1;
 
-  print("hart1: entering initial WFI (should gate clock)\n");
-  wfi();
+    print("hart1: entering initial WFI (should gate clock)\n");
+    wfi();
 
-  print("hart1: woke up from WFI due to MSIP\n");
-  clear_msip();
+    print("hart1: woke up from WFI due to MSIP\n");
+    clear_msip();
   
-  print("hart1: doing some work after wake\n");
-  do_some_work(1);
-  hart1_awake_and_worked = 1;
+    print("hart1: doing some work after wake\n");
+    do_some_work(1);
+    hart1_awake_and_worked = 1;
 
-  print("hart1: waiting for hart0 to request second sleep\n");
-  while (!hart1_sleep_again_request) {
-  }
+    print("hart1: waiting for hart0 to request second sleep\n");
+    while (!hart1_sleep_again_request) {
+        // spin
+    }
 
-  print("hart1: clearing MSIP and going back to WFI (second sleep)\n");
-  clear_msip();
-  wfi(); 
+    print("hart1: clearing MSIP and going back to WFI (second sleep)\n");
+    clear_msip();
+    wfi(); 
 
-  hart1_sleeping_again = 1;
-  print("hart1: went back to sleep; staying in low-power loop\n");
+    hart1_sleeping_again = 1;
+    print("hart1: went back to sleep; staying in low-power loop\n");
 
-  return;
+    return;
 }
