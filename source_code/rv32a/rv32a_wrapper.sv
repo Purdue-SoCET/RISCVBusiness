d8099139b6d135cd6ad636f5a83d3925b6372c2e
Eshan Mathur <mathure@asicfab.ecn.purdue.edu>
2026-04-13 20:17:27 -0400

    fixed wrapper

--- source_code/rv32a/rv32a_wrapper.sv
+++ source_code/rv32a/rv32a_wrapper.sv
@@ -2,8 +2,19 @@
 
 module rv32a_wrapper(
     input CLK,
-    input nRST
+    input nRST,
+    input logic amo_en,
+    input logic mem_ready,
+    input logic [3:0] alu_op,
+    input logic [31:0] mem_output, rs2_data,
+    output logic stall_amo_en,
+    output logic read_mem_en, 
+    output logic write_mem_en,
+    output logic [31:0] mem_input, 
+    output logic [31:0] writeback_data
 );
+
+
     `ifdef RV32A_SUPPORTED
     rv32a_enabled RV32A(.*);
     `else
