121decc4540ac7a73394a79d2345c5a3abb31979
Preston <perkinsp@purdue.edu>
2026-04-19 14:36:04 -0400

    added alu operation decode

--- source_code/rv32a/rv32a_alu.sv
+++ source_code/rv32a/rv32a_alu.sv
@@ -1,28 +1,45 @@
 
 module rv32a_alu(
     input logic [31:0] portA, portB,
-    input logic [3:0] alu_op, //need to change to type
+    input logic [4:0] alu_op, //need to change to type
     output logic [31:0] output_port
 ); 
 
+typedef enum logic [4:0] {
+    AMO_ADD  = 5'b00000,
+    AMO_SWAP = 5'b00001,
+    AMO_LR   = 5'b00010,
+    AMO_SC   = 5'b00011,
+    AMO_XOR  = 5'b00100,
+    AMO_OR   = 5'b01000,
+    AMO_AND  = 5'b01100,
+    AMO_MIN  = 5'b10000,
+    AMO_MAX  = 5'b10100,
+    AMO_MINU = 5'b11000,
+    AMO_MAXU = 5'b11100
+} rv32a_op_e;
+
+rv32a_op_e op;
+assign op = rv32a_op_e'(alu_op);
+
 always_comb begin
 
-    casez(alu_op)
+    casez(op)
        
-        ALU_ADD: output_port = portA + portB; //ADD    rd, rs1, rs2      R[rd] <= R[rs1] + R[rs2]
+        AMO_ADD: output_port = portA + portB; //ADD    rd, rs1, rs2      R[rd] <= R[rs1] + R[rs2]
         
-        ALU_SWAP: output_port = portB;
+        AMO_SWAP: output_port = portB;
 
-        ALU_AND: output_port = portA & portB; //AND    rd, rs1, rs2      R[rd] <= R[rs1] AND R[rs2]
+        AMO_AND: output_port = portA & portB; //AND    rd, rs1, rs2      R[rd] <= R[rs1] AND R[rs2]
 
-        ALU_OR: output_port = portA | portB; //OR     rd, rs1, rs2      R[rd] <= R[rs1] OR R[rs2]
+        AMO_OR: output_port = portA | portB; //OR     rd, rs1, rs2      R[rd] <= R[rs1] OR R[rs2]
 
-        ALU_XOR: output_port = portA ^ portB; //XOR    rd, rs1, rs2      R[rd] <= R[rs1] XOR R[rs2]
+        AMO_XOR: output_port = portA ^ portB; //XOR    rd, rs1, rs2      R[rd] <= R[rs1] XOR R[rs2]
 
-        ALU_MAX:    output_port = ($signed(portA) > $signed(portB)) ? portA : portB;
-        ALU_MAXU:   output_port = (portA > portB) ? portA : portB;
-        ALU_MIN:    output_port = ($signed(portA) < $signed(portB)) ? portA : portB;
-        ALU_MINU:   output_port = (portA < portB) ? portA : portB;
+        AMO_MAX:    output_port = ($signed(portA) > $signed(portB)) ? portA : portB;
+        AMO_MAXU:   output_port = (portA > portB) ? portA : portB;
+        AMO_MIN:    output_port = ($signed(portA) < $signed(portB)) ? portA : portB;
+        AMO_MINU:   output_port = (portA < portB) ? portA : portB;
 
         default: output_port = '0;
 
